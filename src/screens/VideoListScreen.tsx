d0afa27d783f87177d628ccd1e97c18c5e56cb43
Chuks Ben <codetraxx10@gmail.com>
2022-09-19 13:22:00 +0100

    about to add loader and vision camera

--- src/screens/VideoListScreen.tsx
+++ src/screens/VideoListScreen.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {useSelector} from 'react-redux';
+
+import DeviceInfo from 'react-native-device-info';
+import PagerView from 'react-native-pager-view';
+import Video from 'react-native-video';
 
 import {useGetAllVideosQuery} from '../redux/apis';
 import {selectAllVideos} from '../redux/slices';
@@ -11,18 +15,52 @@ const VideoListScreen = () => {
     refetchOnMountOrArgChange: true,
     refetchOnFocus: true,
   });
+  const [paused, setPaused] = useState(true);
 
   const videos = useSelector(selectAllVideos);
 
+  console.log(videos);
+
+  useEffect(() => {
+    DeviceInfo.getUniqueId().then(id => {
+      console.log(id);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text variant="headlineMedium">VideoListScreen</Text>
+      <PagerView
+        style={styles.pagerView}
+        orientation="vertical"
+        initialPage={0}>
+        {videos.map(item => (
+          <View key={item.id} style={{backgroundColor: '#000'}}>
+            <Video
+              poster={item.thumbnailUrl}
+              source={{
+                uri: item.shortUrl,
+              }}
+              style={StyleSheet.absoluteFill}
+              controls={false}
+              repeat
+              paused={paused}
+              onError={error => {
+                console.log(error, 'error+loading_video');
+              }}
+            />
+          </View>
+        ))}
+      </PagerView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+
+  pagerView: {
     flex: 1,
   },
 });
